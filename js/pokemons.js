26413bce6f19feef8a6736d26dd3e72c677dff97
cedric.beau <cedric.beau@boticinal.com>
2022-05-30 11:54:01 +0200

    Update Pokemons component

--- js/pokemons.js
+++ js/pokemons.js
@@ -1,10 +1,15 @@
 export default () => ({
 
   pokemons: [],
-
+  pokemonDetails: [],
+  searchPokemon: 'Rechercher par numéro',
   isLoading: false,
 
   init () {
+    this.displayPokemons();
+  },
+
+  displayPokemons () {
     this.isLoading = true;
     fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
     .then(response => {
@@ -13,9 +18,34 @@ export default () => ({
     })
     .then(response => {
       this.isLoading = false;
-      //this.pokemons = response.items.map(item => item.volumeInfo)
-      this.pokemons = response
-      console.log(this.pokemons)
+      this.pokemons = response.results.map(pokemon => pokemon)
     })
-  }
+  },
+
+  displayPokemonDetails () {
+    const searchPokemon = this.searchPokemon;
+    const url = `https://pokeapi.co/api/v2/pokemon/${searchPokemon}/`;
+    let HTMLstr = '';
+
+    fetch(url)
+    .then(response => {
+      if (!response.ok) alert(`Something went wrong: ${response.spokemontatus} - ${response.statusText}`)
+      return response.json()
+    })
+    .then(response => {
+      this.pokemonDetails = response
+      console.log('detail : ', this.pokemonDetails)
+
+
+      HTMLstr += `<div><img src="${this.pokemonDetails.sprites.front_default}"></div><div><strong>name :</strong> ${this.pokemonDetails.name}</div><div><strong>height :</strong> ${this.pokemonDetails.height}</div>`;
+      document.getElementById('detail').innerHTML = HTMLstr;
+    })
+
+
+  },
+
+  resetField () {
+    this.searchPokemon = '';
+  },
+
 });
