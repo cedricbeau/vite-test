a699f4e75effe48f3fc891709f811597cc1f0c55
cedric.beau <cedric.beau@boticinal.com>
2022-05-31 13:44:22 +0200

    Update Pokemons component

--- js/pokemons.js
+++ js/pokemons.js
@@ -2,64 +2,103 @@ export default () => ({
 
   pokemons: [],
   pokemonDetails: [],
-  searchPokemon: 'Rechercher par numéro',
-  isLoading: false,
+  searchPokemon: 'Rechercher par id ou par nom',
+  show: false,
 
   init () {
     this.displayPokemons();
   },
 
+  /**
+   * Display all pokemons (name only)
+   */
   displayPokemons () {
-    this.isLoading = true;
     fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
     .then(response => {
       if (!response.ok) alert(`Something went wrong: ${response.status} - ${response.statusText}`)
       return response.json()
     })
     .then(response => {
-      this.isLoading = false;
       this.pokemons = response.results.map(pokemon => pokemon)
     })
   },
 
-  displayPokemonDetails () {
+  /**
+   * Handle Search Submit
+   */
+  searchPokemonDetails () {
     const searchPokemon = this.searchPokemon;
     const url = `https://pokeapi.co/api/v2/pokemon/${searchPokemon}/`;
+
+    this.displayPokemonDetails(url);
+  },
+
+  /**
+   *
+   * @param {String} url
+   * Display pokemon details
+   */
+  displayPokemonDetails (url) {
     let HTMLstr = '';
+    let abilitiesStr = '';
+    let statsStr = '';
+    let typesStr = '';
 
     fetch(url)
     .then(response => {
       if (!response.ok) alert(`Something went wrong: ${response.spokemontatus} - ${response.statusText}`)
       return response.json()
     })
+
     .then(response => {
-      this.pokemonDetails = response
-      console.log('detail : ', this.pokemonDetails)
+      this.show = true;
+      this.pokemonDetails = response;
 
+      /**
+       * Main infos
+       */
+      HTMLstr += `<img src="${this.pokemonDetails.sprites.other.home.front_default}" width="256" height="256"><div class="flex flex-wrap justify-between"><span class="pr-4 font-medium text-slate-400">nom :</span> ${this.pokemonDetails.name}</div><div class="flex flex-wrap justify-between"><span class="pr-4 font-medium text-slate-400">taille :</span> ${this.pokemonDetails.height}</div><div class="flex flex-wrap justify-between"><span class="pr-4 font-medium text-slate-400">poids :</span> ${this.pokemonDetails.weight}</div><div class="flex flex-wrap justify-between"><span class="pr-4 font-medium text-slate-400">Expérience :</span> ${this.pokemonDetails.base_experience}</div>`;
 
-      HTMLstr += `<div><img src="${this.pokemonDetails.sprites.front_default}" width="96" height="96"></div><div><strong>name :</strong> ${this.pokemonDetails.name}</div><div><strong>height :</strong> ${this.pokemonDetails.height}</div>`;
       document.getElementById('detail').innerHTML = HTMLstr;
+
+      /**
+       * Types
+       */
+      const types = this.pokemonDetails.types;
+
+      types.forEach(type => {
+        typesStr += `<div class="flex flex-wrap justify-between"><div><span class="pr-4 font-medium text-slate-400">Slot ${type.slot} :</span></div><div>${type.type.name}</div></div>`;
+      });
+
+      document.getElementById('types').innerHTML = typesStr;
+
+      /**
+       * Abilities
+       */
+      const abilities = this.pokemonDetails.abilities;
+
+      abilities.forEach((ability, index) => {
+        abilitiesStr += `<div class="flex flex-wrap justify-between"><span class="pr-4 font-medium text-slate-400">Compétence ${index + 1} :</span> ${ability.ability.name}</div>`
+      });
+
+      document.getElementById('abilities').innerHTML = abilitiesStr;
+
+      /**
+       * Stats
+       */
+      const stats = this.pokemonDetails.stats;
+
+      stats.forEach(stat => {
+        statsStr += `<div class="flex flex-wrap justify-between"><span class="pr-4 font-medium text-slate-400">${stat.stat.name} :</span> ${stat.base_stat}</div>`;
+      });
+
+      document.getElementById('stats').innerHTML = statsStr;
     })
   },
 
-  displayDetails (url) {
-    let HTMLstr = '';
-
-    fetch(url)
-    .then(response => {
-      if (!response.ok) alert(`Something went wrong: ${response.spokemontatus} - ${response.statusText}`)
-      return response.json()
-    })
-    .then(response => {
-      this.pokemonDetails = response
-      console.log('detail : ', this.pokemonDetails)
-
-      HTMLstr += `<div><img src="${this.pokemonDetails.sprites.front_default}" width="96" height="96"></div><div><strong>name :</strong> ${this.pokemonDetails.name}</div><div><strong>height :</strong> ${this.pokemonDetails.height}</div><div><strong>weight :</strong> ${this.pokemonDetails.weight}</div>`;
-
-      document.getElementById('detail').innerHTML = HTMLstr;
-    })
-  },
-
+  /**
+   * Empty search field
+   */
   resetField () {
     this.searchPokemon = '';
   },
