98b80c806f36c8bbe38287873b278073b175313a
cedric.beau <cedric.beau@boticinal.com>
2022-05-31 14:03:19 +0200

    Update Fetch books components

--- js/books.js
+++ js/books.js
@@ -1,17 +1,14 @@
 export default () => ({
 
   books: [],
-  isLoading: false,
 
   init () {
-    this.isLoading = true;
     fetch('https://www.googleapis.com/books/v1/volumes?q=Alpine')
     .then(response => {
       if (!response.ok) alert(`Something went wrong: ${response.status} - ${response.statusText}`)
       return response.json()
     })
     .then(response => {
-      this.isLoading = false;
       this.books = response.items.map(item => item.volumeInfo)
     })
   }
